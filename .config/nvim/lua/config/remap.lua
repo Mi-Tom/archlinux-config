a21da25d71d56b4cfc6644142770340f978f9625
M. Tomanec <mtomanec7@gmail.com>
2026-07-05 16:02:09 +0200

    doplněny zkratky pro neovim

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -1,3 +1,7 @@
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { noremap = true })
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>w", vim.cmd.w, { noremap = true })
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { noremap = true, desc = "Open file explorer" })
+vim.keymap.set({"i", "v"}, "jk", "<Esc>", { noremap = true, silent = true, desc = "Exit to normal mode" })
+vim.keymap.set("n", "<leader>w", vim.cmd.w, { noremap = true, desc = "Save file" })
+vim.keymap.set("n", "<leader>s", vim.cmd.split, { noremap = true, desc = "Horizintal split" })
+vim.keymap.set("n", "<leader>v", vim.cmd.vsplit, { noremap = true, desc = "Horizintal split" })
+vim.keymap.set("n", "<leader>x", vim.cmd.close, {noremap = true, desc = "Close window"})
+
