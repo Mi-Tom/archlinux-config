f32ce56b5b66b107ad40f97e8eece638e6d417fe
M. Tomanec <mtomanec7@gmail.com>
2026-07-14 13:00:53 +0200

    neovim configurace prepsana na lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,2 @@
-require("config.options")
-require("config.remap")
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,32 +1,3 @@
-local opt = vim.opt
-
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.g.netrw_keepdir = 0
-
-opt.updatetime = 250
-
-opt.termguicolors = true
-opt.signcolumn = "yes"
-opt.fillchars = { vert = "│"}
-
-opt.number = true
-
-opt.cursorline = true
-opt.colorcolumn = "100"
-opt.scrolloff = 10
-
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-
-opt.hlsearch = false
-opt.incsearch = true
-
-opt.swapfile = false
-opt.backup = false
-opt.undofile = true
-opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
